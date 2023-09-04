6115b5255062bbf0206360327d95520903a6a900
paritoshratawal <paritoshratawal@gmail.com>
2023-09-04 19:39:26 +0530

    Modified files and implement router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { createRoot } from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+
+import Router from './Router';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Router/>
+  </React.StrictMode>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
