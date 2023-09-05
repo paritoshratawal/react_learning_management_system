0a619382b4ad3ce77c821b91c97dbae3f4871777
paritoshratawal <paritoshratawal@gmail.com>
2023-09-05 12:50:26 +0530

    Add components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-// import './index.css';
-// import App from './App';
 
 import Router from './Router';
 import * as serviceWorker from './serviceWorker';
